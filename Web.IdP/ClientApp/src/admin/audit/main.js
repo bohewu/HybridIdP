d1ad4bf6adbe0cd5365b4fd8423dfd20e5cbbd27
BoHeWu <bohewu@gmail.com>
2025-11-28 12:35:08 +0800

    refactor: register v-loading directive in audit main.js
    - Add v-loading directive to audit/main.js
    - Now all admin SPAs have v-loading directive registered

--- Web.IdP/ClientApp/src/admin/audit/main.js
+++ Web.IdP/ClientApp/src/admin/audit/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import AuditApp from './AuditApp.vue'
 import './style.css'
 import i18n from '@/i18n'
+import vLoading from '@/directives/v-loading'
 
 const app = createApp(AuditApp)
 app.use(i18n)
+app.directive('loading', vLoading)
 app.mount('#app')
