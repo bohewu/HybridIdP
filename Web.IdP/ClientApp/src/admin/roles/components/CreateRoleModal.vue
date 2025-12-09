90a566dfc9acb948168d9bd7f30aa27bf84585f0
BoHeWu <bohewu@gmail.com>
2025-12-09 11:14:20 +0800

    fix: add missing useI18n import in role modals

--- Web.IdP/ClientApp/src/admin/roles/components/CreateRoleModal.vue
+++ Web.IdP/ClientApp/src/admin/roles/components/CreateRoleModal.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
 import BaseModal from '../../../components/common/BaseModal.vue'
+
+const { t } = useI18n()
 
 const emit = defineEmits(['close', 'created'])
 
