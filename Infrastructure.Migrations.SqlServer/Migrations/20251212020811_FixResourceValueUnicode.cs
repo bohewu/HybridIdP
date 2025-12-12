46c961bd5a5fc18c7a81574d7011b3e8abfbdecb
BoHeWu <bohewu@gmail.com>
2025-12-12 10:59:40 +0800

    fix(db): support unicode in resource value and update e2e test with chinese characters

---
+++ Infrastructure.Migrations.SqlServer/Migrations/20251212020811_FixResourceValueUnicode.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixResourceValueUnicode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Resources",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Resources",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
