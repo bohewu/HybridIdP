60179cf3bd4d7fa3dc4df7059c413e8f9c54b9f9
BoHeWu <bohewu@gmail.com>
2025-12-12 20:45:10 +0800

    fix(i18n): update i18n paths in Vue components to profile.common.xxx
    - EditProfileForm.vue: common.xxx -> profile.common.xxx
    - ChangePasswordForm.vue: common.xxx -> profile.common.xxx
    - Module namespace requires full path including filename prefix

--- Web.IdP/ClientApp/src/profile/components/ChangePasswordForm.vue
+++ Web.IdP/ClientApp/src/profile/components/ChangePasswordForm.vue
@@ -84,14 +84,14 @@
           @click="resetForm"
           class="px-4 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50"
         >
-          {{ t('common.cancel') }}
+          {{ t('profile.common.cancel') }}
         </button>
         <button 
           type="submit"
           :disabled="loading"
           class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed"
         >
-          {{ loading ? t('common.saving') : t('common.save') }}
+          {{ loading ? t('profile.common.saving') : t('profile.common.save') }}
         </button>
       </div>
     </form>
@@ -206,11 +206,11 @@ const handleSubmit = async () => {
       } else if (data.error) {
         errors.value.push(data.error)
       } else {
-        errors.value.push(t('common.errorOccurred'))
+        errors.value.push(t('profile.common.errorOccurred'))
       }
     }
   } catch (err) {
-    errors.value.push(t('common.networkError'))
+    errors.value.push(t('profile.common.networkError'))
     console.error('Failed to change password:', err)
   } finally {
     loading.value = false
