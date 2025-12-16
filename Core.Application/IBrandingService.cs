0d92ac3b4361fadd6b80ddb3b6ae76b01bd0eb5e
BoHeWu <bohewu@gmail.com>
2025-12-16 15:24:40 +0800

    feat(branding): Add configurable Copyright and PoweredBy settings
    Backend:
    - Add Copyright/PoweredBy to BrandingOptions, SettingKeys
    - Add GetCopyrightAsync/GetPoweredByAsync to IBrandingService
    - Implement in BrandingService with defaults
    
    Layouts updated to use IBrandingService:
    - _Layout.cshtml, _AdminLayout.cshtml, _ApplicationManagerLayout.cshtml
    
    Login pages now use _GoogleFooter partial:
    - Login.cshtml, LoginMfa.cshtml, Register.cshtml
    
    Admin UI:
    - Add Copyright/PoweredBy fields to BrandingSettings.vue
    - Add i18n translations (en-US, zh-TW)

--- Core.Application/IBrandingService.cs
+++ Core.Application/IBrandingService.cs
@@ -7,4 +7,6 @@ public interface IBrandingService
 {
     Task<string> GetAppNameAsync(CancellationToken ct = default);
     Task<string> GetProductNameAsync(CancellationToken ct = default);
+    Task<string> GetCopyrightAsync(CancellationToken ct = default);
+    Task<string?> GetPoweredByAsync(CancellationToken ct = default);
 }
