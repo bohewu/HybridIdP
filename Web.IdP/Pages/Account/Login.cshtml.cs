697b2206a0b3e70db6c9bbe1d3252cc27441cdf5
BoHeWu <bohewu@gmail.com>
2025-12-22 15:19:06 +0800

    fix(security): address CodeQL security findings
    - Fix log injection: sanitize newlines in search term logging
    - Fix cookie security: add Secure=true and SameSite=Lax to locale cookies
      - Login.cshtml.cs
      - ProfileManagementController.cs
      - LanguageController.cs
    - Fix open redirect: add safeRedirect() validation in razor.js
    
    Note: AuthorizationController.cs Request.Method check is safe (not user-controlled)

--- Web.IdP/Pages/Account/Login.cshtml.cs
+++ Web.IdP/Pages/Account/Login.cshtml.cs
@@ -345,7 +345,7 @@ public partial class LoginModel : PageModel
                     Response.Cookies.Append(
                         Microsoft.AspNetCore.Localization.CookieRequestCultureProvider.DefaultCookieName,
                         Microsoft.AspNetCore.Localization.CookieRequestCultureProvider.MakeCookieValue(new Microsoft.AspNetCore.Localization.RequestCulture(preferredLocale)),
-                        new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                        new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1), Secure = true, SameSite = SameSiteMode.Lax }
                     );
                 }
 
