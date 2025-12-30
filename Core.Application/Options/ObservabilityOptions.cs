ac51b155f01323d2f4fef344c6b2c6e7e3556f5e
BoHeWu <bohewu@gmail.com>
2025-12-30 13:04:06 +0800

    Disable OpenTelemetry by default to reduce console noise

--- Core.Application/Options/ObservabilityOptions.cs
+++ Core.Application/Options/ObservabilityOptions.cs
@@ -5,6 +5,7 @@ public class ObservabilityOptions
     public const string MonitoringSection = "Monitoring";
     public const string ObservabilitySection = "Observability";
 
+    public bool Enabled { get; set; } = true;
     public string MetricsBaseUrl { get; set; } = "https://localhost:7035";
     public List<string> AllowedIPs { get; set; } = new();
     public bool PrometheusEnabled { get; set; } // Added
