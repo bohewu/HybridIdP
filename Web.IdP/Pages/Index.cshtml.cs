6bf53354d05bbe146a5521a695a2678a7cf1ef14
BoHeWu <bohewu@gmail.com>
2025-12-31 16:27:01 +0800

    UI: Fix Homepage to use Branding:ProductName instead of generic text

--- Web.IdP/Pages/Index.cshtml.cs
+++ Web.IdP/Pages/Index.cshtml.cs
@@ -7,11 +7,15 @@ namespace Web.IdP.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly Web.IdP.Options.BrandingOptions _brandingOptions;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, Microsoft.Extensions.Options.IOptions<Web.IdP.Options.BrandingOptions> brandingOptions)
     {
         _logger = logger;
+        _brandingOptions = brandingOptions.Value;
     }
+
+    public string ProductName => _brandingOptions.ProductName;
 
     public void OnGet()
     {
