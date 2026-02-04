1bfc0995635a1efdedae08970ae486696f95b86c
BoHeWu <bohewu@gmail.com>
2026-02-04 16:02:06 +0800

    feat: remove Support link, create public Docs page with i18n
    - Removed Support link from Welcome page footer
    - Created public Docs page explaining HybridAuth IdP features
    - Added comprehensive documentation covering: what is IdP, how to use, protocols, security
    - Added 30 i18n keys for Docs page (en-US, zh-TW)
    - Docs page is public, no authentication required

---
+++ Web.IdP/Pages/Docs.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.IdP.Pages;
+
+public class DocsModel : PageModel
+{
+    public void OnGet()
+    {
+        // Public documentation page - no authentication required
+    }
+}
